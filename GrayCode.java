d575c7762a28d3f15ff5bef44a7b660a1efbe225
messi-wpy <904315105@qq.com>
2019-06-06 23:39:49 +0800

    6.6-graycode-89

---
+++ GrayCode.java
@@ -0,0 +1,26 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GrayCode {
+
+    public static void main(String[] args) {
+        System.out.println(grayCode(2));
+    }
+    public static List<Integer> grayCode(int n) {
+        List<Integer>list=new ArrayList<>();
+        list.add(0);
+
+        for (int i = 0; i <n ; i++) {
+            int size=list.size();
+            for (int j = size-1; j >=0 ; j--) {
+                list.add(list.get(j)|(1<<i));
+            }
+        }
+
+    return list;
+    }
+}
+
+
