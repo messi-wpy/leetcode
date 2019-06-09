a5ece3bc538aaaf6e5e3ae22e31e444d257a145e
messi-wpy <904315105@qq.com>
2019-06-10 00:07:43 +0800

    fix

--- NQueens.java
+++ NQueens.java
@@ -1,11 +1,13 @@
 package leetcode;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class NQueens {
 
     public List<List<String>> solveNQueens(int n) {
+        List<List<String>>res=new ArrayList<>();
         int queens[]=new int[n];
         for (int i = 0; i <queens.length ; i++) {
             queens[i]=-1;
@@ -14,11 +16,34 @@ public class NQueens {
         while (k>=0){
 
             queens[k]++;
-            while ()
-
+            while (queens[k]<n&&isConflict(k,queens)){
+                queens[k]++;
+            }
+            if (queens[k]<n&&k==n-1){
+                List<String>temp=new ArrayList<>();
+                for (int i = 0; i <n ; i++) {
+                    StringBuilder sb=new StringBuilder();
+                    for (int j = 0; j <n ; j++) {
+                        if (j==queens[i])
+                            sb.append('Q');
+                        else
+                            sb.append('.');
+                    }
+                    temp.add(sb.toString());
+                }
+                res.add(temp);
+            }
+            else if (queens[k]<n&&k<n-1){
+                k++;
+            }
+            else {
+                queens[k] = -1;
+                k--;
+            }
 
 
         }
+        return res;
     }
 
     public boolean isConflict(int k,int [] queens){
