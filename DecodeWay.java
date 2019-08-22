c548ea81688d764ffe01261fb5fa58a624016b8c
messi-wpy <904315105@qq.com>
2019-08-22 19:14:42 +0800

    dp 91

---
+++ DecodeWay.java
@@ -0,0 +1,18 @@
+package leetcode;
+
+public class DecodeWay {
+
+    public int numDecodings(String s) {
+        int f[]=new int[s.length()+1];
+        f[0]=1;
+        for (int i = 1; i <=s.length() ; i++) {
+            if (s.charAt(i-1)!='0'){
+                f[i]=f[i-1];
+            }
+            if (i>1&&s.substring(i-2,i).compareTo("10")>=0&&s.substring(i-2,i).compareTo("26")<=0){
+                f[i]+=f[i-2];
+            }
+        }
+        return f[s.length()];
+    }
+}
