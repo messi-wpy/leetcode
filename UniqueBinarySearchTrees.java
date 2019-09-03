84e472aad894062030adfa1acd22eeb07d92c743
messi-wpy <904315105@qq.com>
2019-09-03 23:13:18 +0800

    102-tree

--- UniqueBinarySearchTrees.java
+++ UniqueBinarySearchTrees.java
@@ -3,7 +3,16 @@ package leetcode;
 public class UniqueBinarySearchTrees {
 
     public int numTrees(int n) {
-        return 0;
+        if (n==0)return 0;
+        int[] dp =new int[n+1];
+        dp[0]=dp[1]=1;
+
+        for (int i = 2; i <=n ; i++) {
+            for (int j = 1; j <=i ; j++) {
+                dp[i]+=dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
     }
 
 
