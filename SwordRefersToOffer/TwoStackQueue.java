905c5c9541864da63fb3c93e91974ec4d6fae9c1
messi-wpy <904315105@qq.com>
2019-12-02 00:01:01 +0800

    stack

---
+++ SwordRefersToOffer/TwoStackQueue.java
@@ -0,0 +1,18 @@
+package exercise.SwordRefersToOffer;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class TwoStackQueue {
+
+    Deque<Integer> stack1 = new ArrayDeque<Integer>();
+    Deque<Integer> stack2 = new ArrayDeque<Integer>();
+
+    public void push(int node) {
+
+    }
+
+    public int pop() {
+
+    }
+}
