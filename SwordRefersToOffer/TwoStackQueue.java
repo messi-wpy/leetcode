06bb3e06b78ba06e4194a4baf427e97291e8e3e3
messi-wpy <904315105@qq.com>
2019-12-02 00:08:38 +0800

    queue

--- SwordRefersToOffer/TwoStackQueue.java
+++ SwordRefersToOffer/TwoStackQueue.java
@@ -9,10 +9,18 @@ public class TwoStackQueue {
     Deque<Integer> stack2 = new ArrayDeque<Integer>();
 
     public void push(int node) {
-
+        stack1.push(node);
     }
 
     public int pop() {
-
+        if (!stack2.isEmpty()){
+            return stack2.pop();
+        }
+        if (stack1.isEmpty())
+            throw new RuntimeException("stack is empty");
+        while (!stack1.isEmpty()){
+            stack2.push(stack1.pop());
+        }
+        return stack2.pop();
     }
 }
