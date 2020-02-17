1fd4732e5331d26791ab3c65ae80151760b3a46c
messi-wpy <904315105@qq.com>
2020-02-17 22:47:49 +0800

    linkedlist

---
+++ SwordRefersToOffer/test.java
@@ -0,0 +1,11 @@
+package exercise.SwordRefersToOffer;
+
+public class test {
+    public static void main(String[] args) {
+        int time=0;
+        time=(int)(System.currentTimeMillis()/1000);
+        System.out.println(time);
+        System.out.println((int)System.currentTimeMillis());
+        System.out.println(System.currentTimeMillis());
+    }
+}
